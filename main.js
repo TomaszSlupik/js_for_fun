5e416658d954220b04b9147b76de82ad1c2d5db4
TomaszSlupik <slupiktomasz@gmail.com>
2024-04-06 09:24:20 +0200

    js

--- main.js
+++ main.js
@@ -46,16 +46,95 @@ console.log('---')
 
 // zwykła posiada argumenty a strzałkowa NIE POSIADA
 // W zwykłej funkcji będziemy mieć wynik a w strzałkowej: ReferenceError: arguments is not defined
+
 function yourNumber () {
     console.log(arguments)
 }
 
 yourNumber(1, 2, 3)
 
-const yourNumberTwo = () => {
-    console.log(arguments)
+// const yourNumberTwo = () => {
+//     console.log(arguments)
+// }
+
+// yourNumberTwo(1, 2, 3)
+
+// 4. JS jest językiem synchronicznym, a wszystkie setTimeouty, fetch, axios, BTN - addEventListener itp. tworzą język ten ASYNCHRONICZNYM 
+// setTimeout są to metody Window czyli od Node.js 
+// Po to jest asynchroniczność aby np. przeglądarka nie musiała czekać na jakieś akcje, np. pobieranie przez API pogody i żeby 
+// strona mogła działać bez problemu 
+// Przykład: Tutaj setTimeout wykona się na samym końcu
+console.log('Start')
+
+let numb = [1,2,3,4]
+
+function iterateArray(numb) {
+    for (let i = 0; i < numb.length; i++) {
+        console.log(i)
+    }
 }
 
-yourNumberTwo(1, 2, 3)
+iterateArray(numb)
+
+setTimeout(() => {
+    console.log('test')
+}, 3000)
+
+
+console.log('stop')
+
+// 2 przykład 
+// Mamy kod, on trafia na STOS, 
+// wszytsko co jest asynchroniczne czyli setTimeouty, API pogody trafią na WEB API, potem one trafiają do KOLEJKI 
+// EVENT LOOP sa to mechanizmy które cały czas krążą i sprawdzają czy STOS jest pusty => gdy stos jest pusty wtedy wszytskie funkcje z kolejki trafiają na STOS 
+
+
+
+// 5. FILTER, MAP, REDUCE 
+const myNm = [1, 2, 3, 4, 5, 6, 7, 8]
+
+// filter - tworzy nową tablicę zawierającą tylko te elementy oryginalnej tablicy, które spełniają określony warunek,
+// służy do filtrowania elementów tablicy na podstawie określonego warunku
+const myNumFilter = myNm.filter((el) => el % 2 == 0)
+console.log(myNumFilter)
+
+// map - tworzy nową tablicę, służy do transformacji każdego elementu 
+const myNumMap = myNm.map((el) => el * 10)
+console.log(myNumMap)
+
+// Reduce - redukuje tablicę do jednego elementu, ostatni parametr to od jakiej liczby ma zacząc liczyć 
+const myNumReduce = myNm.reduce((prev, cur) => prev + cur, 0)
+console.log(myNumReduce)
+
+
+// 6. DODAWANIE, EDYCJA, USUWANIE
+
+// dodawanie najlepsze DESTRUKTURYZACJA 
+const sport = ['pływanie', 'rower']
+const allSport = [...sport, 'bieganie']
+
+console.log(allSport)
+
+// edycja 
+const sportEdit = sport.map((el) => el === 'pływanie' ? 'zamiana na rower' : el)
+console.log(sportEdit)
+
+// usuwanie 
+const sportDelete = sport.filter((el) => el !== 'pływanie')
+console.log(sportDelete)
+
+
+// 7. callback 
+//  Callbacki są często używane w operacjach asynchronicznych, gdy dana funkcja już zakończyła się wtedy funkcja asynchroniczna jest odpalana 
+function greate(yourName, callback) {
+    console.log(`Hej, co tam słychać ${yourName}`)
+    callback()
+}
+
+ function goodbye () {
+    console.log('Cześć')
+ }
+
+greate('Tomek', goodbye)
 
 
